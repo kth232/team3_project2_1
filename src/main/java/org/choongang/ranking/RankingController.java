40574589134ccc00eb38e50ca466c4c6fe26d71a
hyeon12 <seojh912_@naver.com>
2024-05-28 17:22:00 +0900

    랭킹 수정중

--- src/main/java/org/choongang/ranking/RankingController.java
+++ src/main/java/org/choongang/ranking/RankingController.java
@@ -1,13 +1,7 @@
 package org.choongang.ranking;
 
-import com.sun.tools.javac.Main;
-import org.apache.ibatis.session.SqlSession;
 import org.choongang.global.AbstractController;
-import org.choongang.global.Router;
-import org.choongang.global.configs.DBConn;
 import org.choongang.global.contents.Menu;
-import org.choongang.main.MainRouter;
-import org.choongang.member.mapper.MemberMapper;
 import org.choongang.template.Templates;
 
 public class RankingController extends AbstractController {
@@ -15,5 +9,6 @@ public class RankingController extends AbstractController {
     public void show() {
         Templates.getInstance().render(Menu.RANKING);
     }
+    
 
 }
