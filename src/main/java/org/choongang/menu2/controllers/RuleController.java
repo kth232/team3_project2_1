2c1f3650583bffeb9c6657a0a19ef8b96a6f4391
heyejiyang <xoxo99312@naver.com>
2024-05-28 20:39:03 +0900

    menu2-1 controller 수정

--- src/main/java/org/choongang/menu2/controllers/RuleController.java
+++ src/main/java/org/choongang/menu2/controllers/RuleController.java
@@ -19,8 +19,34 @@ public class RuleController extends AbstractController {
 
     @Override
     public void prompt() {
+        System.out.print("게임시작(4) or 종료(q): ");
+        String menu = sc.nextLine();
+        if (menu.equals("q") || menu.equals("quit") || menu.equals("exit")) {
+            System.out.println("프로그램을 종료합니다.");
+            System.out.println(Templates.getInstance().doubleLine());
+            System.exit(0); //0: 정상종료/1: 비정상 종료
+        }
+        try {
+            int m = Integer.parseInt(menu);
+            change(m); //라우터를 통해 메뉴 변경
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("메뉴는 [숫자]로 입력해주세요.");
+        }
+
         Router2 router = Menu2Router.getInstance();
         router.change(Menu2.GAMESTART);
         //추후 게임화면으로 넘어가는 라우터 연결
     } //prompt 재정의
+    private void change(int menuNo) {
+        Menu2 menu2 = null;
+        switch (menuNo) {
+            case 4:
+                menu2 = Menu2.GAMESTART;
+                break; //
+            default:
+                prompt();
+        }
+        Menu2Router.getInstance().change(menu2);
+    }
 }
